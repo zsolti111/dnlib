1cedbd2154248f1ca467584c0ff40e9f1c203ed9
Zsolti <aaaa>
2017-02-13 12:53:33 +0100

    check myclass, count properties, and functions

--- Examples/Example1.cs
+++ Examples/Example1.cs
@@ -1,40 +1,51 @@
 ﻿using System;
 using dnlib.DotNet;
+using System.Threading;
 
-namespace dnlib.Examples {
-	// This example will open mscorlib.dll and then print out all types
-	// in the assembly, including the number of methods, fields, properties
-	// and events each type has.
-	public class Example1 {
-		public static void Run() {
-			// Load mscorlib.dll
-			string filename = typeof(void).Module.FullyQualifiedName;
-			ModuleDefMD mod = ModuleDefMD.Load(filename);
+namespace dnlib.Examples
+{
+    // This example will open mscorlib.dll and then print out all types
+    // in the assembly, including the number of methods, fields, properties
+    // and events each type has.
+    public class Example1
+    {
+        public static void Run ()
+        {
+            // Load mscorlib.dll
+            //////////string filename = typeof(void).Module.FullyQualifiedName;
 
-			int totalNumTypes = 0;
-			// mod.Types only returns non-nested types.
-			// mod.GetTypes() returns all types, including nested types.
-			foreach (TypeDef type in mod.GetTypes()) {
-				totalNumTypes++;
-				Console.WriteLine();
-				Console.WriteLine("Type: {0}", type.FullName);
-				if (type.BaseType != null)
-					Console.WriteLine("  Base type: {0}", type.BaseType.FullName);
+            string filename = "C:\\Users\\Zsolti\\Desktop\\önlab\\MyClassLibrary\\MyClassLibrary\\MyClassLibrary\\bin\\Debug\\MyClassLibrary.dll";
+            ModuleDefMD mod = ModuleDefMD.Load(filename);
 
-				Console.WriteLine("  Methods: {0}", type.Methods.Count);
-				Console.WriteLine("  Fields: {0}", type.Fields.Count);
-				Console.WriteLine("  Properties: {0}", type.Properties.Count);
-				Console.WriteLine("  Events: {0}", type.Events.Count);
-				Console.WriteLine("  Nested types: {0}", type.NestedTypes.Count);
+            int totalNumTypes = 0;
+            // mod.Types only returns non-nested types.
+            // mod.GetTypes() returns all types, including nested types.
+            foreach (TypeDef type in mod.GetTypes())
+            {
 
-				if (type.Interfaces.Count > 0) {
-					Console.WriteLine("  Interfaces:");
-					foreach (InterfaceImpl iface in type.Interfaces)
-						Console.WriteLine("    {0}", iface.Interface.FullName);
-				}
-			}
-			Console.WriteLine();
-			Console.WriteLine("Total number of types: {0}", totalNumTypes);
-		}
-	}
+                Thread.Sleep(2000);
+
+                totalNumTypes++;
+                Console.WriteLine();
+                Console.WriteLine("Type: {0}", type.FullName);
+                if (type.BaseType != null)
+                    Console.WriteLine("  Base type: {0}", type.BaseType.FullName);
+
+                Console.WriteLine("  Methods: {0}", type.Methods.Count);
+                Console.WriteLine("  Fields: {0}", type.Fields.Count);
+                Console.WriteLine("  Properties: {0}", type.Properties.Count);
+                Console.WriteLine("  Events: {0}", type.Events.Count);
+                Console.WriteLine("  Nested types: {0}", type.NestedTypes.Count);
+
+                if (type.Interfaces.Count > 0)
+                {
+                    Console.WriteLine("  Interfaces:");
+                    foreach (InterfaceImpl iface in type.Interfaces)
+                        Console.WriteLine("    {0}", iface.Interface.FullName);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Total number of types: {0}", totalNumTypes);
+        }
+    }
 }
---
+++ Examples/gsdgf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnlib.Examples
+{
+	class gsdgf
+	{
+
+		void f ()
+		{
+			ssfdfdfdf
+		}
+
+
+	}
+}
